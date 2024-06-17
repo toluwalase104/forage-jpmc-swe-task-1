611b20db237fb38754822882b1d4709c1280affa
Osazoname <59113736+toluwalase104@users.noreply.github.com>
2024-06-17 17:23:28 +0100

    Update client3.py
    getRatio() and getDataPoint() now work as intended

--- client3.py
+++ client3.py
@@ -21,6 +21,7 @@
 import json
 import random
 import urllib.request
+from math import inf as INFINITY
 
 # Server API URLs
 QUERY = "http://localhost:8080/query?id={}"
@@ -35,25 +36,38 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (ask_price + bid_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return INFINITY
+    
+    return price_a / price_b
 
 
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
     for _ in iter(range(N)):
-        quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+        # quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+        try:
+            response = urllib.request.urlopen(QUERY.format(random.random())).read()
+            quotes = json.loads(response)
+        except json.JSONDecodeError:
+            print("Failed to decode JSON response")
+            continue
+        except urllib.error.URLError as e:
+            print(f"Failed to make request: {e}")
+            continue
 
         """ ----------- Update to get the ratio --------------- """
+        stock_prices = dict()
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            stock_prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
-
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(stock_prices["ABC"], stock_prices["DEF"]))
